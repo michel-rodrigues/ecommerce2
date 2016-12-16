c87a9aee627dc69218fa08e8c6e851a62a03af9b
Michel <michel.rodrigues86@yahoo.com.br>
2016-12-16 14:39:28 -0200

    073 ManyToMany Through

--- source/carts/models.py
+++ source/carts/models.py
@@ -5,6 +5,8 @@ from products.models import Variation
 
 
 class  CartItem(models.Model):
+    # o argumento é uma string pois a classe só será definida abaixo
+    cart = models.ForeignKey('Cart')
     item = models.ForeignKey(Variation)
     quantity = models.PositiveIntegerField(default=1)
 
@@ -14,7 +16,8 @@ class  CartItem(models.Model):
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-    items = models.ManyToManyField(CartItem)
+    # 'through' recebe o modelo intermediário
+    items = models.ManyToManyField(Variation, through=CartItem)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
