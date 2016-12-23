83d49fd21af3364387cc094e79e1c06e1c7e834e
Michel <michel.rodrigues86@yahoo.com.br>
2016-12-23 10:45:47 -0200

    097 Order Model

--- source/orders/models.py
+++ source/orders/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from carts.models import Cart
+
 
 class UserCheckout(models.Model):
     user = models.OneToOneField(
@@ -30,3 +32,15 @@ class UserAddress(models.Model):
 
     def __str__(self):
         return self.street
+
+
+class Order(models.Model):
+    cart = models.ForeignKey(Cart)
+    user = models.ForeignKey(UserCheckout)
+    shipping_address = models.ForeignKey(UserAddress, related_name='shipping_address')
+    billing_address = models.ForeignKey(UserAddress, related_name='billing_address')
+    shipping_total_price = models.DecimalField(decimal_places=2, max_digits=10, default=5.99)
+    order_total = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return str(self.cart.id)
